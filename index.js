97902dfc6fc63bc41e7d8044e3bf33e29435c970
Orangalo <yuvalgat11@gmail.com>
2016-10-28 14:52:20 +0300

    Updated index.js
    Changed the sum function, still under development.

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 var Gat =
 	module.exports = {
-		sum: (arr) => {
-			var ret = 0;
-			for (var i = 0; i < arr.length; i++) {
-				ret += arr[i];
+		sum: () => {
+			var result = 0;
+			for (val of arguments) {
 			}
-			return ret;
 		},
 		average: (arr) => {
-			var ret = Gat.sum(arr) / arr.length;
-			return ret;
 		},
-		// avg: Gat.average,
 		factorial: (n) => {
-			var ret = 1;
 			for (var i = 1; i <= n; i++) {
-				ret *= i;
 			}
-			return ret;
 		}
-
 	}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
 var Gat = require('../index.js');
+
 var arr = [1, 2, 3, 4];
 var n = 5;
-console.log(Gat.sum(arr));
-console.log(Gat.average(arr));
-console.log(Gat.factorial(n));
-// console.log(Gat.avg(arr));
+//
+// console.log(`Gat.sum(${JSON.stringify(arr)}): ${Gat.sum(arr)}`);
+// console.log(`Gat.average(${JSON.stringify(arr)}): ${Gat.average(arr)}`);
+// console.log(`Gat.factorial(${n}): ${Gat.factorial(n)}`);
+//
+console.log(Gat.sum([1, 2, 3, 4]));
+console.log(Gat.test(10));
