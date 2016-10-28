a93ecc91d5452afe016c9b906d9e8cd472e3d82f
Orangalo <yuvalgat11@gmail.com>
2016-10-28 19:47:56 +0300

    Updated index.js
    Changed the sum function; now accepts arrays, strings, and single
    numbers.

--- index.js
+++ index.js
@@ -2,35 +2,25 @@ var Gat =
 	module.exports = {
 		sum: () => {
 			var result = 0;
-			for (val of arguments) {
-				// var err = new Error(`Argument number ${args.indexOf(val)} isn't valid.`);
-				console.log(val);
 				switch (typeof val) {
 					case "number":
-						console.log("NUMBER: " + val);
 						result += val;
 						break;
 					case "string":
-						console.log("STRING: " + val);
 						var num = parseInt(val);
 						if (!isNaN(num)) {
 							result += num;
 						}
 						break;
 					case "object":
-						console.log(`OBJECT`);
-						console.log(val);
 						if (Array.isArray(val)) {
-							console.log("ARRAY");
 							for (val2 of val) {
 								result += Gat.sum(val2);
 							}
 						}
 						break;
 					default:
-						throw new Error();
 				}
-				return result;
 			}
 		},
 		average: (arr) => {
@@ -43,8 +33,5 @@ var Gat =
 				result *= i;
 			}
 			return result;
-		},
-		test: (n) => {
-			console.log(this);
 		}
 	}
--- test/test.js
+++ test/test.js
@@ -1,11 +1,8 @@
 var Gat = require('../index.js');
 
-var arr = [1, 2, 3, 4];
+var arr = [1, 2, 3, [4, 5]];
 var n = 5;
-//
-// console.log(`Gat.sum(${JSON.stringify(arr)}): ${Gat.sum(arr)}`);
-// console.log(`Gat.average(${JSON.stringify(arr)}): ${Gat.average(arr)}`);
-// console.log(`Gat.factorial(${n}): ${Gat.factorial(n)}`);
-//
-console.log(Gat.sum([1, 2, 3, 4]));
-console.log(Gat.test(10));
+var nStr = "13";
+console.log(Gat.sum(arr));
+console.log(Gat.average(arr));
+console.log(Gat.factorial(n));
