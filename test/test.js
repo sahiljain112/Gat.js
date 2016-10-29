38d42c9ae8b310d7fe868533a8a30361e1b45566
Orangalo <yuvalgat11@gmail.com>
2016-10-29 22:52:50 +0300

    Add test for cube root function to test/test.js

--- test/test.js
+++ test/test.js
@@ -14,3 +14,4 @@ console.log("\nSigma: i = 3 to 5, i ^ 2: " + Gat.sigma(3, 5, (i) => {
 console.log("\nQuadratic solution for (a, b, c) -> (1, -1, -2): " + Gat.quadratic(1, -1, -2));
 console.log("\nAdding Complex number (3 + 4i) with Complex number (5 - 2i) (Code: (new Gat.Complex(3, 4).add(new Gat.Complex(5, -2)))) (Note: this code returns an object, I took out the real and imaginary part in order to print them.): " + cStr)
 console.log("\nChecking wheather 73 is prime or not (Code: Gat.isPrime(73)): " + Gat.isPrime(73));
+console.log("\nCube root of 8: " + Gat.cbrt(8));
